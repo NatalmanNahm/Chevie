49e24210d48739eebb3a198899abdfde51e473ea
Natalman <sperobb@outlook.com>
2019-12-24 19:53:01 -0600

    Edit the Home page so that the user can logout and go back to the login page. And change the password field of the register page to be dot password like

--- app/src/main/java/com/example/chevie/HomeActivity.java
+++ app/src/main/java/com/example/chevie/HomeActivity.java
@@ -2,13 +2,36 @@ package com.example.chevie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button mLogOutBtn;
+    FirebaseAuth mFirebaseAuth;
+    private Context mContext;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mLogOutBtn = (Button) findViewById(R.id.logOut_btn);
+        mContext = this;
+        mLogOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(mContext, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
