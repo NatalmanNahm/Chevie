ea1e1ef3a4e459eeda64827039982d5a78e449c3
Natalman <natalnahm@gmail.com>
2019-12-27 20:51:57 -0600

    change the recyclerView dependency to use androidx instead of v7. Just made the newsAdapter to extend recyclerview with a viewHolder

--- app/src/main/java/com/example/chevie/Adapters/NewsAdapter.java
+++ app/src/main/java/com/example/chevie/Adapters/NewsAdapter.java
@@ -1,4 +1,31 @@
 package com.example.chevie.Adapters;
 
-public class NewsAdapter extends {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewholder> {
+    @NonNull
+    @Override
+    public NewsViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NewsViewholder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class NewsViewholder extends RecyclerView.ViewHolder {
+        public NewsViewholder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
