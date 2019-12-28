d97766798f0a89dcd88875480d17adaa8e55d72c
Natalman <natalnahm@gmail.com>
2019-12-28 12:26:24 -0600

    added Picasso library dependency to gradle build. modified the news adapter to make a use of the data we will get from the api call and the new Item list xml file to populate the UI.

--- app/src/main/java/com/example/chevie/Adapters/NewsAdapter.java
+++ app/src/main/java/com/example/chevie/Adapters/NewsAdapter.java
@@ -1,31 +1,97 @@
 package com.example.chevie.Adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.chevie.Models.News;
+import com.example.chevie.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewholder> {
+
+    //initializer
+    ArrayList<News> mNews = new ArrayList<>();
+    Context mContext;
+
+    /**
+     * Constructor for the adapter
+     * @param context
+     * @param news
+     */
+    public NewsAdapter(Context context, ArrayList<News> news){
+        mContext = context;
+        mNews = news;
+    }
+
+
     @NonNull
     @Override
     public NewsViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        Context context = parent.getContext();
+        int layoutListItem = R.layout.news_list_items;
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = layoutInflater.inflate(layoutListItem, parent, shouldAttachToParentImmediately);
+        return new NewsViewholder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull NewsViewholder holder, int position) {
-
+    public void onBindViewHolder(@NonNull NewsViewholder newsHolder, int position) {
+        newsHolder.bindNews(mNews.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (mNews == null) return 0;
+        return mNews.size();
     }
 
+    //Creating a setter for the news
+    public void setmNews(ArrayList<News> mNews) {
+        this.mNews = mNews;
+        notifyDataSetChanged();
+    }
+
+    /**
+     * Cache of the children views for a news list Item
+     */
     public class NewsViewholder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.news_title) TextView mNewsTitle;
+        @Bind(R.id.news_time_shared) TextView mTime;
+        @Bind(R.id.source) TextView mSource;
+        @Bind(R.id.news_content) TextView mNewsContent;
+        @Bind(R.id.news_player_img) ImageView mPlayerImg;
+        Context mContext;
+
+
         public NewsViewholder(@NonNull View itemView) {
             super(itemView);
+            mContext = itemView.getContext();
+            ButterKnife.bind(this, itemView);
+        }
+
+        //This is to populate texts and image for the news UI
+        public void bindNews(News news){
+            mNewsTitle.setText(news.getmTitle());
+            mTime.setText(news.getmTimeShared());
+            mSource.setText(news.getmSource());
+            mNewsContent.setText(news.getmContent());
+            Picasso.get().load(news.getmPlayerPic()).into(mPlayerImg);
+
         }
     }
 }
