b7d0f89d54440624c67cb4766bfb646529c7fd3e
Natalman <natalnahm@gmail.com>
2020-01-01 11:53:52 -0600

    Create a new class that just serves as model to info of a news.

--- app/src/main/java/com/example/chevie/Models/NewsInfo.java
+++ app/src/main/java/com/example/chevie/Models/NewsInfo.java
@@ -1,4 +1,50 @@
 package com.example.chevie.Models;
 
+/**
+ * News info model
+ */
+
 public class NewsInfo {
+
+    private int mNewsPlayerId;
+    private String mSource;
+    private String mTimeShared;
+    private String mTitle;
+    private String mContent;
+
+    /**
+     * Constructor for the news info to be used to create the actual news object
+     * @param playerId
+     * @param source
+     * @param time
+     * @param title
+     * @param content
+     */
+    public NewsInfo(int playerId, String source, String time, String title, String content){
+        mNewsPlayerId = playerId;
+        mSource = source;
+        mTimeShared = time;
+        mTitle = title;
+        mContent = content;
+    }
+
+    public int getmNewsPlayerId() {
+        return mNewsPlayerId;
+    }
+
+    public String getmSource() {
+        return mSource;
+    }
+
+    public String getmTimeShared() {
+        return mTimeShared;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public String getmContent() {
+        return mContent;
+    }
 }
