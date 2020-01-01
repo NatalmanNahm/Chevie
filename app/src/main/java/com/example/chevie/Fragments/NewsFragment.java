bd78206a95ae621b7a30b4a8c3f38cbbace56ca2
Natalman <natalnahm@gmail.com>
2020-01-01 12:22:52 -0600

    The NewsFragment now can get news data that will soon be displayed to the User.

--- app/src/main/java/com/example/chevie/Fragments/NewsFragment.java
+++ app/src/main/java/com/example/chevie/Fragments/NewsFragment.java
@@ -1,111 +1,147 @@
 package com.example.chevie.Fragments;
 
-import android.content.Context;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.chevie.Adapters.NewsAdapter;
+import com.example.chevie.Models.News;
+import com.example.chevie.Models.NewsInfo;
+import com.example.chevie.Models.PlayerProfile;
 import com.example.chevie.R;
+import com.example.chevie.Utilities.NetworkUtils;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link NewsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link NewsFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class NewsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // the fragment initialization parameters
+    private View rootView;
+    private RecyclerView mNewsRecyclerView;
+    private TextView mNewserrorMessage;
+    private LinearLayoutManager mNewsLLManager;
+    private NewsAdapter mNewsAdapter;
+    private ArrayList<News> mNewsArrayList;
+    private ArrayList<NewsInfo> mInfoArray;
+    private ArrayList<PlayerProfile> mPlayerProf;
+    int mPlayerId;
+    String mContent;
+    String mTitle;
+    String mTime;
+    String mSource;
+    String mPhotoUrl;
+    String mShortName;
 
-    private OnFragmentInteractionListener mListener;
 
     public NewsFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NewsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NewsFragment newInstance(String param1, String param2) {
-        NewsFragment fragment = new NewsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
-    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+        rootView = inflater.inflate(R.layout.fragment_news, container, false);
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+        //Getting reference to the recyclerview
+        mNewsRecyclerView = (RecyclerView) rootView.findViewById(R.id.news_recyclerView);
+        mNewserrorMessage = (TextView) rootView.findViewById(R.id.error_message);
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+        //Creating a Linear Layout manager
+        mNewsLLManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+
+        mNewsRecyclerView.setLayoutManager(mNewsLLManager);
+        mNewsRecyclerView.setHasFixedSize(true);
+        mNewsAdapter = new NewsAdapter(getContext(), mNewsArrayList);
+        mNewsRecyclerView.setAdapter(mNewsAdapter);
+
+        new FetchNewsData().execute();
+
+        return rootView;
     }
 
     /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
+     * simple method to show news data on the ui
      */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    private void showNewsDataView() {
+        /* First, make sure the error is invisible */
+        mNewserrorMessage.setVisibility(View.INVISIBLE);
+        /* Then, make sure the news data is visible */
+        mNewsRecyclerView.setVisibility(View.VISIBLE);
     }
+
+    /**
+     * Simple method to show error when needed.
+     */
+    private void showNewsErrorMessage() {
+        /* First, hide the currently visible data */
+        mNewsRecyclerView.setVisibility(View.INVISIBLE);
+        /* Then, show the error */
+        mNewserrorMessage.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * AsyncTask to get news Data
+     */
+    public class FetchNewsData extends AsyncTask<String, Void, ArrayList<News>>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected ArrayList<News> doInBackground(String... strings) {
+
+            //Just getting the news info
+            mInfoArray = NetworkUtils.fetchNews();
+
+            //Iterating through the arraylist of news info
+            //Then create a news Arraylist
+            for (int i = 0; i < mInfoArray.size(); i++){
+                NewsInfo newsInfo = mInfoArray.get(i);
+                mPlayerId = newsInfo.getmNewsPlayerId();
+                mSource = newsInfo.getmSource();
+                mTime = newsInfo.getmTimeShared();
+                mContent = newsInfo.getmContent();
+                mTitle = newsInfo.getmTitle();
+
+                //getting player profile info
+                mPlayerProf = NetworkUtils.fetchPlayerProfile(mPlayerId);
+                PlayerProfile playerProfile = mPlayerProf.get(0);
+                mPhotoUrl = playerProfile.getmPlayerImg();
+                mShortName = playerProfile.getmShortName();
+
+                mNewsArrayList.add(new News(mPlayerId, mShortName, mSource, mTime,
+                        mTitle, mContent, mPhotoUrl));
+
+            }
+
+            return mNewsArrayList;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<News> news) {
+
+            if (news != null && !news.isEmpty()){
+                showNewsDataView();
+                mNewsAdapter.setmNews(news);
+            } else {
+                showNewsErrorMessage();
+            }
+            super.onPostExecute(news);
+        }
+    }
+
 }
