37b5f4000228e0e69ee9fa4f08effd9a2d0d2c0c
Natalman <natalnahm@gmail.com>
2020-01-24 20:05:34 -0600

    AllTeamActivity is now able to inflate gridview of all teams with RecyclerView.

--- app/src/main/java/com/example/chevie/AllTeamActivity.java
+++ app/src/main/java/com/example/chevie/AllTeamActivity.java
@@ -1,14 +1,94 @@
 package com.example.chevie;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
-public class AllTeamActivity extends AppCompatActivity {
+import com.example.chevie.Adapters.AllTeamsAdapter;
+import com.example.chevie.Models.Teams;
+import com.example.chevie.Utilities.NetworkUtils;
+
+import java.util.ArrayList;
+
+/**
+ * All teams Activity
+ * Show all teams in the league
+ */
+
+public class AllTeamActivity extends AppCompatActivity implements AllTeamsAdapter.TeamsOnClickHandler{
+
+    //Initializer
+    RecyclerView mRecyclerView;
+    AllTeamsAdapter mAdapter;
+    ArrayList<Teams> mTeams = new ArrayList<>();
+    GridLayoutManager mGridLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_team);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.teams_recyclerView);
+        mGridLayoutManager = new GridLayoutManager(this, calculateNoOfColumns(this),
+                GridLayoutManager.VERTICAL, false);
+
+        mRecyclerView.setLayoutManager(mGridLayoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        mAdapter = new AllTeamsAdapter(mTeams, getApplicationContext(), this);
+        mRecyclerView.setAdapter(mAdapter);
+
+        new FetchAllTeams().execute();
+
+    }
+
+    /**
+     * AsyncTask to get all teams from the Api call
+     */
+    public class FetchAllTeams extends AsyncTask<String, Void, ArrayList<Teams>>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected ArrayList<Teams> doInBackground(String... strings) {
+            mTeams = NetworkUtils.fetchTeams();
+            return mTeams;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Teams> teams) {
+            super.onPostExecute(teams);
+            if (teams != null ){
+                mAdapter.setmTeams(teams);
+            }
+        }
+    }
+
+    /**
+     * Helper method to calculate number of columns to be displayed
+     * @param context
+     * @return
+     */
+    public static int calculateNoOfColumns(Context context) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+        int scalingFactor = 200;
+        int noOfColumns = (int) (dpWidth / scalingFactor);
+        if(noOfColumns < 3)
+            noOfColumns = 3;
+        return noOfColumns;
+    }
+
+    @Override
+    public void onClick(Teams teams) {
+
     }
 }
