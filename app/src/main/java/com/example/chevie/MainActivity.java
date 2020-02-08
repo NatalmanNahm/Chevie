5b8aa38ea0e5a8e5f6a1996a8fc9356b54342820
Natalman <natalnahm@gmail.com>
2020-02-07 21:25:19 -0600

    added a new style.xml file for level api 21. Modified the MainActivity to do some basic stuff like adding toolbar and handler backPressed.

--- app/src/main/java/com/example/chevie/MainActivity.java
+++ app/src/main/java/com/example/chevie/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.chevie;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set default toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //this is for menu bar to unfold and fold back in on pressed
+        mDrawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        mDrawer.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
+
+    /**
+     * If the drawer/menu bar is open on back press close the drawer
+     * else proceed as usual
+     */
+    @Override
+    public void onBackPressed() {
+        if (mDrawer.isDrawerOpen(GravityCompat.START)){
+            mDrawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
 }
